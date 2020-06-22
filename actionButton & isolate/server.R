4239b3faceb9b86cb2f6e72277145c7d24879c83
YanZhangNZ <crazyanz888@gmail.com>
2020-06-22 20:40:39 +1200

    actionButton and isolate for dynamic renderTable

--- actionButton & isolate/server.R
+++ actionButton & isolate/server.R
@@ -16,4 +16,27 @@ server <- function(input,output){
   })
   
   
+  #for dynamic observations of selected dataset
+
+  output$datasetname <- renderText({
+    paste("Structure of the dataset is", input$dataset)
+  })
+  
+  output$structure <- renderPrint({
+    str(get(input$dataset))
+  })
+  
+  output$observations <- renderText({
+    input$update
+    isolate(paste("first",input$num,"observations of the dataset",input$dataset))
+  })
+
+  output$view <- renderTable({
+    
+    #capture the button click event
+    input$update
+    #isolate() is to create dependency on the action button
+    isolate(head(get(input$dataset),n=input$num))
+    
+  })
 }
--- actionButton & isolate/ui.R
+++ actionButton & isolate/ui.R
@@ -10,17 +10,33 @@ ui <- fluidPage(
       textInput("lastname","Enter your last name"),
       #action button to upate input info
       actionButton("action","Update last name"),
-      p("click on the update button to update last name")
+      p("click on the update button to update last name"),
       
       
       # Vs. submitButton("Submit")
+      
+      
+      #dynamiclly update/view observation of selected dataset
+      selectInput("dataset","Select a dataset",c("iris","trees","mtcars")),
+      numericInput("num","Number of observations",6),
+      
+      actionButton("update","Click to change observation of selected dataset")
+      
+      
      ),
     
     mainPanel(
       textOutput("outputfirstname"),
-      textOutput("outputlastname")
+      textOutput("outputlastname"),
+      
+      
+      #for dynamic observations
+      h4(textOutput("datasetname")),
+      verbatimTextOutput("structure"),
+      
+      h4(textOutput("observations")),
+      tableOutput("view")
 
     )
   )
 )
-
