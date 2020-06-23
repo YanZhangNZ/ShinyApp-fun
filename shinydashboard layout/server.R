627b7a20dfc360ba4f7ddfa8ee423b2a0f4e0715
YanZhangNZ <crazyanz888@gmail.com>
2020-06-23 23:39:21 +1200

    grid with box function and dynamical plot

--- shinydashboard layout/server.R
+++ shinydashboard layout/server.R
@@ -1,7 +1,41 @@
 library(shiny)
+library(shinydashboard)
+library(datasets)
+library(plotly)
 
-
-server <- function(input,output){
+server <- function(input,output,session){
+ #output data table for the 2nd row
+  output$data <- renderTable({
+    head(mtcars)
+  })
+  
+  #scatter plot of the 1st row
+  output$plot1 <- renderPlotly({
+    plot_ly(
+      data=mtcars,
+      x=~wt,
+      y=~mpg,
+      type="scatter",
+      mode="markers"
+    )
+  })
+  
  
+  
+  #dynamically render input widget for the 2nd row
+  output$inputwidget <- renderUI({
+    selectInput("selected","Select a variable",names(iris))
+  })
+  
+  #histogram plot for the 1st row
+  output$plot2 <- renderPlotly({
+    
+    plot_ly(
+      data=iris,
+      #x=~Sepal.Length,
+      x=~get(input$selected),
+      type="histogram"
+    )
+  })
       
 }
--- shinydashboard layout/ui.R
+++ shinydashboard layout/ui.R
@@ -1,5 +1,6 @@
 library(shiny)
 library(shinydashboard)
+library(plotly)
 
 ui <- dashboardPage(
   dashboardHeader(title = "this is header", titleWidth = 500),
@@ -11,9 +12,11 @@ ui <- dashboardPage(
         "Chart",
         tabName = "chart", 
         icon = icon("line-chart"),
+        #downlist of sub menu
         menuSubItem("chartmenusub1",tabName = "chart1",icon = icon("line-chart")),
         menuSubItem("chartmenusub2",tabName = "chart2",icon = icon("line-chart"))
       ),
+      menuSubItem("box",tabName = "box",icon = icon("check")),
       menuItem("Link",href="https://rstudio.io",icon=icon("code"))
     )
   ),
@@ -27,6 +30,35 @@ ui <- dashboardPage(
       tabItem(
         tabName = "data",
         dataTableOutput("mydatatable")
+      ),
+      
+      #grid for body
+      tabItem(
+        tabName = "box",
+        #first row, having two boxes of same height, same width by default
+        fluidRow(
+          box(
+            title = "Box with a plot",
+            plotlyOutput("plot1",height = 250)
+          ),
+          box(
+            title = "Box with another plot", 
+            plotlyOutput("plot2",height=250)
+          )
+        ),
+        #second row, of different width
+        fluidRow(
+          box(
+            title="Box with datatable",
+            tableOutput("data"),
+            width = 8
+          ),
+          box(
+            title="Box with input widget",
+            uiOutput("inputwidget"),
+            width = 4
+          )
+       ),
       )
     )
   )
