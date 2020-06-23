4e8aa250a84492f16187abc2190ad961522820b7
YanZhangNZ <crazyanz888@gmail.com>
2020-06-23 21:58:17 +1200

    menu content and sub menus

--- shinydashboard layout/ui.R
+++ shinydashboard layout/ui.R
@@ -7,8 +7,27 @@ ui <- dashboardPage(
     sidebarMenu(
       menuItem(text="About",tabName="about",icon=icon("clipboard")),
       menuItem("Data",tabName = "data", icon=icon("database"),badgeLabel = "new", badgeColor = "green"),
+      menuItem(
+        "Chart",
+        tabName = "chart", 
+        icon = icon("line-chart"),
+        menuSubItem("chartmenusub1",tabName = "chart1",icon = icon("line-chart")),
+        menuSubItem("chartmenusub2",tabName = "chart2",icon = icon("line-chart"))
+      ),
       menuItem("Link",href="https://rstudio.io",icon=icon("code"))
     )
   ),
-  dashboardBody("this is body")
+  dashboardBody(
+    tabItems(
+      tabItem(
+        tabName = "about",
+        p("this is a demo of shinydashboard"),
+        p("we will use iris dataset")
+      ),
+      tabItem(
+        tabName = "data",
+        dataTableOutput("mydatatable")
+      )
+    )
+  )
 )
