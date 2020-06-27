d7b45fabbc5a1b85c009d71df742ab9c4a3b67f8
YanZhangNZ <crazyanz888@gmail.com>
2020-06-27 19:04:06 +1200

    connectivity with local MSSQL Server

---
+++ dashboard with external SQL database/server.R
@@ -0,0 +1,156 @@
+library(shiny)
+library(shinydashboard)
+library(datasets)
+library(plotly)
+library(scatterplot3d)
+library(RODBC)
+
+
+
+
+
+dbconnection <- odbcDriverConnect(
+  "Driver=SQL Server;
+  Server=localhost;
+  Database=MPTT;
+  trusted_connection=yes"
+)
+data <- sqlFetch(
+  dbconnection, 
+  'student_profile', 
+  colnames=F, 
+  rows_at_time=1000
+)
+
+
+server <- function(input,output,session){
+  
+
+  #scatter plot of the 1st row
+  output$plot1 <- renderPlotly({
+    
+    plot_ly(
+      data=mtcars,
+      x=~wt,
+      y=~mpg,
+      type="scatter",
+      mode="markers"
+    )
+  })
+  
+ 
+  
+  #dynamically render input widget for the 2nd row
+  output$inputwidget <- renderUI({
+    selectInput("selected","Select a variable",names(iris))
+  })
+  
+  #histogram plot for the 1st row
+  output$plot2 <- renderPlotly({
+    
+    plot_ly(
+      data=iris,
+      #x=~Sepal.Length,
+      x=~get(input$selected),
+      type="histogram"
+    )
+  })
+  
+  #3d scatter plot for the 2nd row
+  output$scatter <- renderPlot({
+    scatterplot3d(Soils[,c(13,6,14)],highlight.3d = T,pch=16,angle = 50,type="h",
+                  xlab = "Sodium content",
+                  ylab = 'Soil pH',
+                  zlab = 'Conductivity')
+  })
+  
+  #line chart for the 2nd row
+  output$linechart <- renderPlotly({
+    plot_ly(
+      data=data.frame(Soils), 
+      x = ~Group,  
+      y = ~Na,
+      type = 'scatter', 
+      mode = 'lines', 
+      color = ~Contour , 
+      colors = c("green","darkred","orange")) 
+  })
+  
+  #infobox output
+  output$min_ <- renderInfoBox({
+    infoBox(
+      title = "Minm",
+      value = min(mtcars$mpg),
+      subtitle = "minimum value of mpg",
+      fill = T
+    )
+  })
+  output$max_ <- renderInfoBox({
+    infoBox(
+      title = "Maxm",
+      value = max(mtcars$mpg),
+      subtitle = "maxmum value of mpg",
+      fill = T,
+      color = "green"
+    )
+  })
+  output$median_ <- renderInfoBox({
+    infoBox(
+      title = "Medn",
+      value = median(mtcars$mpg),
+      subtitle = "median value of mpg",
+      icon = icon("usd"),
+      fill = T,
+      color = "yellow"
+    )
+  })
+  output$mean_ <- renderInfoBox({
+    infoBox(
+      title = "Mean",
+      value = mean(mtcars$mpg),
+      icon = icon("usd"),
+      subtitle = "Mean value of mpg",
+      fill = T,
+      color = "purple"
+    )
+  })
+  
+  
+  #valuebox output
+  output$min <- renderValueBox({
+    valueBox(
+      value = min(mtcars$mpg),
+      subtitle = "minimum value of mpg",
+      icon("arrow-up"),
+    )
+  })
+  output$max <- renderValueBox({
+    valueBox(
+      value = max(mtcars$mpg),
+      subtitle = "maxmum value of mpg",
+      color = "green"
+    )
+  })
+  output$median <- renderValueBox({
+    valueBox(
+      value = median(mtcars$mpg),
+      subtitle = "median value of mpg",
+      color = "yellow"
+    )
+  })
+  output$mean <- renderValueBox({
+    valueBox(
+      value = mean(mtcars$mpg),
+      subtitle = "mean value of mpg",
+      icon("angle-double-right"),
+      color = "purple"
+    )
+  })
+
+  
+  #external sql database table output
+  output$mydatatable <- renderTable({
+    data[1:10,1:10]
+  })
+
+}
---
+++ dashboard with external SQL database/ui.R
@@ -0,0 +1,147 @@
+library(shiny)
+library(shinydashboard)
+library(plotly)
+library(shinycssloaders)
+library(shinyBS) # for popover
+
+ui <- dashboardPage(
+  dashboardHeader(
+    title = tags$a(href="https://rstudio.com/", icon("bar-chart"),"this is a demo of dashboard"),
+    titleWidth = 500,
+    tags$li(class="dropdown",tags$a(href="https://rstudio.com/",icon("youtube"),"Videos ",target="_blank")),
+    tags$li(class="dropdown",tags$a(href="https://rstudio.com/",icon("linkedin"),"Information ")),
+    tags$li(class="dropdown",tags$a(href="https://rstudio.com/",icon("github"),"Source Code "))
+  ),
+  
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem(text="About",tabName="about",icon=icon("clipboard")),
+      menuItem("Data",tabName = "data", icon=icon("database"),badgeLabel = "new", badgeColor = "green"),
+      menuItem(
+        "Chart",
+        tabName = "chart", 
+        icon = icon("line-chart"),
+        #downlist of sub menu
+        menuSubItem("infobox",tabName = "infobox",icon = icon("line-chart")),
+        menuSubItem("valuebox",tabName = "valuebox",icon = icon("line-chart"))
+      ),
+      menuSubItem("box",tabName = "box",icon = icon("check")),
+      menuItem("Link",href="https://rstudio.io",icon=icon("code"))
+    )
+  ),
+  
+  dashboardBody(
+    tags$head(
+      tags$link(rel="stylesheet", type = "text/css", href = "custom.css")
+    ),
+    #popover -- on mouse hover question icon 
+    bsPopover(
+      id = "q1", title = "Mean",
+      content = "Mean value of mpg",
+      trigger = "hover",
+      placement = "right",
+      options = list(container = "body")
+    ),
+    #popover -- on hover the valuebox
+    bsPopover(
+      id = "median_",
+      title = "Median value of mpg",
+      trigger = "hover",
+      placement = "right",
+      options = list(container = "body")
+    ),
+    
+    tabItems(
+      
+      tabItem(
+        tabName = "about",
+        p("this is a demo of shinydashboard"),
+        p("we will use multiple internal and external dataset")
+      ),
+      
+      
+      tabItem(
+        tabName = "data",
+        tableOutput("mydatatable")
+      ),
+      tabItem(
+        tabName = "infobox",
+        fluidRow(
+          infoBoxOutput("min_",width = 3),
+          infoBoxOutput("max_",width = 3),
+          infoBoxOutput("median_",width = 3),
+          infoBoxOutput("mean_",width = 3)
+        )
+      ),
+      tabItem(
+        tabName = "valuebox",
+          fluidRow(
+            valueBoxOutput("min2",width = 3),
+            valueBoxOutput("max2",width = 3),
+            valueBoxOutput("median2",width = 3),
+            valueBoxOutput("mean2",width = 3)
+          )
+        ),
+      
+      #grid for body
+      tabItem(
+        tabName = "box",
+        
+        #first row for highlighters
+
+            fluidRow(
+              valueBoxOutput("min",width = 3),
+              valueBoxOutput("max",width = 3),
+              valueBoxOutput("median",width = 3),
+              #valueBoxOutput("mean",width = 3),
+              box(
+                #title="Box with input widget",
+                uiOutput("inputwidget"),
+                solidHeader = T,
+                status = "success",
+                width = 3,
+                background = "purple"
+              )
+            ),
+
+        #first row, having two boxes of same height, same width by default
+        fluidRow(
+           box(
+            title = "Box with scatter plot",
+            status = "success",
+            solidHeader = T,
+            withSpinner(plotlyOutput("plot1",height = 250))
+          ),
+         
+         box(
+           title = "Box with histogram plot", 
+           withSpinner(plotlyOutput("plot2",height=250)),
+           status = "success",
+           solidHeader = T,
+           collapsible = T
+         )
+
+        
+        ),
+        
+        #second row, of different width
+        fluidRow(
+          box(
+            title="Box with line chart",
+            plotlyOutput("linechart",height = 280),
+            status = "success",
+            solidHeader = T,
+          ),
+          box(
+            title="Box with 3d plot",
+            plotOutput("scatter",height = 280),
+            solidHeader = T,
+            status = "success",
+            
+          )
+       )
+      )
+    )
+  )
+)
+
